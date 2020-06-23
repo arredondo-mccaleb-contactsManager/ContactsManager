d4ec42592e98065751cc43d2c239f6a7812a2e96
Jay Arredondo <jearredondo13@gmail.com>
2020-06-22 21:44:59 -0500

    Added the view contacts and add contact functionality. Need to refactor into separate methods.

--- src/Contact.java
+++ src/Contact.java
@@ -1,34 +1,36 @@
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class Contact {
 
-    private String name;
-    private long phoneNumber;
+    private String contactInfo;
 
 
-    public Contact(String name, long phoneNumber) {
-        this.name = name;
-        this.phoneNumber = phoneNumber;
+    public Contact(String contactInfo) {
+        this.contactInfo = contactInfo;
     }
 
     public String getName() {
-        return name;
+        return contactInfo;
     }
 
     public void setName(String name) {
-        this.name = name;
+        this.contactInfo = contactInfo;
     }
 
-    public long getPhoneNumber() {
-        return phoneNumber;
+    public static List<String> contactsToContactStrings(List<Contact> contactList) {
+        List<String> contactStrings = new ArrayList<>();
+        for (Contact contact : contactList) {
+            contactStrings.add(contact.getName());
+        }
+        return contactStrings;
     }
-
-    public void setPhoneNumber(long phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-//    HashMap<>
-    public void showAllContacts() {
-
+    public static List<Contact> contactStringsToContacts(List<String> contactStrings) {
+        List<Contact> contacts = new ArrayList<>();
+        for (String contactString : contactStrings) {
+            contacts.add(new Contact(contactString));
+        }
+        return contacts;
     }
 }
